427f42c5337fbc64adbc0852aaec325283d61def
lucasbesen <lucassilvabesen@gmail.com>
2019-08-01 18:06:39 -0300

    Setup routes

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+import Home from '../containers/Home';
+import Heroes from '../containers/Heroes';
+import Hero from '../containers/Hero';
+
+const Routes = () => (
+  <>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/heroes" component={Heroes} />
+    <Route exact path="/hero/:id" component={Hero} />
+  </>
+);
+
+export default Routes;
